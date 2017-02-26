2e124a84e2c1a49d25c13b5ece987faf3448f7e8
gaia012@naver.com <gaia012@naver.com>
2017-02-26 13:09:48 +0900

    chapter2 문제풀이

---
+++ src/test/java/gaia012/java8/chapter2/Apple.java
@@ -0,0 +1,15 @@
+package gaia012.java8.chapter2;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by LeeJongRyul on 2017-02-26.
+ */
+@Getter
+@Setter
+public class Apple {
+    private String color;
+    private int weight;
+
+}
---
+++ src/test/java/gaia012/java8/chapter2/AppleFormatter.java
@@ -0,0 +1,8 @@
+package gaia012.java8.chapter2;
+
+/**
+ * Created by LeeJongRyul on 2017-02-26.
+ */
+public interface AppleFormatter {
+    String accept(Apple apple);
+}
