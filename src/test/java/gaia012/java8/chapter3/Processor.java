19f7ebb6b721a2ff73f2d4a0fe030cb3ce6786dd
gaia012@naver.com <gaia012@naver.com>
2017-03-04 17:25:20 +0900

    chapter3

--- src/test/java/gaia012/java8/chapter3/Processor.java
+++ src/test/java/gaia012/java8/chapter3/Processor.java
@@ -1,10 +1,14 @@
 package gaia012.java8.chapter3;
 
+import com.google.common.collect.Lists;
 import org.junit.Test;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
+import java.util.function.BiPredicate;
+import java.util.function.Function;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
@@ -37,5 +41,34 @@ public class Processor {
         assertThat(twoLines, is("oneLinetwoLine"));
         assertEquals(oneLine, "oneLine");
         assertEquals(twoLines, "oneLinetwoLine");
+    }
+
+    @Test
+    public void execute2() {
+//      람다 안에서 사용되는 지역변수는 final(상수)처럼 사용되어야 한다.
+
+        final int portNumber = 1337;
+        Runnable r = () -> System.out.println(portNumber);
+//        portNumber = 31337;
+        assertThat(portNumber, is(1337));
+    }
+
+    @Test
+    public void execution3(){
+        String str = "3";
+        Function<String, Integer> stringToInteger =
+                (String s) -> Integer.parseInt(s);
+        Function<String, Integer> stringToInteger2 = Integer::parseInt;
+        assertThat(stringToInteger.apply(str), is(stringToInteger2.apply(str)));
+
+        List<String> param = Lists.newArrayList();
+        param.add("1");
+        param.add("2");
+        param.add("3");
+
+        BiPredicate<List<String>, String> contain = (list, element) -> list.contains(element);
+        BiPredicate<List<String>, String> contain2 = List::contains;
+
+        assertThat(contain.test(param, "3"), is(contain2.test(param, "3")));
     }
 }
--- src/test/java/gaia012/java8/chapter3/README.md
+++ src/test/java/gaia012/java8/chapter3/README.md
@@ -25,8 +25,32 @@
 함수 디스크립터
 함수형 인터페이스의 추상 메서드 시그니처
 - Predicate : T -> boolean
-- Consumer : T -> void
+- Consumer : T -> void****
 - Function T -> R
 
+형식검사, 형식추론, 제약
 
+형식검사.
+List<Apple> heavierThan150g = filter(inventory , (Apple a) -> a.getWeight() ) 150);
+
+1. 람다가 사용된 컨텍스트는 무엇인가? filter 정의 확인
+2. 대상형식은 Predicate<Apple>이다.
+3. Predicate<Apple> 인터페이스의 추상메서드는 무엇인가?
+4. Apple을 인수로 받아 boolean을 반환하는 test 메서드다.
+5. 함수디스크립터는 Apple -> boolean이므로 람다의 시그니처와 일치한다.
+ boolean을 반환하므로 코드 형식검사가 성공적으로 완료된다.
+ 
+형식추론
+List<Apple> greenApples = filter(inventory, a -> "green".equals(a.getColor()));
+- a : Apple
+
+지역변수사용
+람다에서 사용되는 지역 변수는 명시적으로 final로 선언되어 있거나 final 변수 처럼 사용되어져야 한다.
+쓰레드의 안정성을 위해서 막아놓음.
+
+클로저
+함수의 비지역 변수를 자유롭게 참조할 수 있는 함수의 인스턴스
+
+메서드 레퍼런스(클래스명::메소드명)
+Apple::getWeight == (apple a) a -> a.getWeight()
 
