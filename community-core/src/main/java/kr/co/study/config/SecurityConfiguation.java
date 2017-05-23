b7a33b0f4ec602c826c657e1f3e64735e87499cf
Taeo(이종렬) <gaia012@coupang.com>
2017-05-23 22:55:41 +0900

    SpringBoot 설정

---
+++ community-core/src/main/java/kr/co/study/config/SecurityConfiguation.java
@@ -0,0 +1,10 @@
+package kr.co.study.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by coupang on 2017. 5. 23..
+ */
+@Configuration
+public class SecurityConfiguation {
+}
