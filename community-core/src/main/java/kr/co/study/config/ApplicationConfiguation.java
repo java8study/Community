a4cc94cca4b81fb5434941001b7dd17a4dee5098
Taeo(이종렬) <gaia012@coupang.com>
2017-05-24 00:19:41 +0900

    SpringBoot 설정

--- community-core/src/main/java/kr/co/study/config/ApplicationConfiguation.java
+++ community-core/src/main/java/kr/co/study/config/ApplicationConfiguation.java
@@ -1,7 +1,15 @@
 package kr.co.study.config;
 
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
+
+import javax.sql.DataSource;
 
 /**
  * Created by coupang on 2017. 5. 23..
@@ -9,5 +17,26 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(value = "kr.co.study")
+@PropertySource(value = { "classpath:properties/datasource.properties" })
 public class ApplicationConfiguation {
+
+	@Autowired
+	private Environment env;
+
+	@Bean(destroyMethod = "close")
+	public DataSource dataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName(env.getProperty("database.DriverClassName"));
+		dataSource.setUrl(env.getProperty("jdbc:mysql://localhost:3306/community"));
+		dataSource.setUsername(env.getProperty("database.UserName"));
+		dataSource.setPassword(env.getProperty("database.Password"));
+		dataSource.setDefaultAutoCommit(false);
+		return dataSource;
+	}
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+
 }
