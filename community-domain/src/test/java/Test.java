f520cff0d594ecb259453cade792ef5c6ba770d6
Taeo(이종렬) <gaia012@coupang.com>
2017-06-04 15:39:31 +0900

    TEST

---
+++ community-domain/src/test/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * Created by coupang on 2017. 6. 4..
+ */
+public class Test {
+	@org.junit.Test
+	public void test12(){
+		Class clazz = kr.co.study.sample.dto.SampleDto.class;
+		String str = "TEST";
+	}
+}
