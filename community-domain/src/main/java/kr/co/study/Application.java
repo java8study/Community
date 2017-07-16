57fc89a7c831523f35f667a1c60d78064b5f9fa4
김연 <jewel1609@naver.com>
2017-07-16 12:21:08 +0900

    page commit

--- community-domain/src/main/java/kr/co/study/Application.java
+++ community-domain/src/main/java/kr/co/study/Application.java
@@ -1,15 +1,15 @@
-package kr.co.study;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-/**
- * Created by coupang on 2017. 5. 23..
- */
-@SpringBootApplication
-public class Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-}
+//package kr.co.study;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+///**
+// * Created by coupang on 2017. 5. 23..
+// */
+//@SpringBootApplication
+//public class Application {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(Application.class, args);
+//	}
+//}
--- community-domain/src/sample-schema.sql
+++ community-domain/src/sample-schema.sql
@@ -3,8 +3,8 @@ id long not null auto_increment,
 userId varchar(45) not null,
 userPwd varchar(80) not null,
 userName varchar(45) not null,
-email varhcar(80) not null
-)
+email varchar(80) not null
+);
 
 insert into sample(userId,userPwd,userName,email) values('A','A','A','A@naver.com'); 
 insert into sample(userId,userPwd,userName,email) values('B','B','B','B@naver.com'); 
