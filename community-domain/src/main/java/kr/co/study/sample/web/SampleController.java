8121bb65abe62ec10c4ac717d6a0d9d2d62182a6
Taeo <gaia012@coupang.com>
2019-02-27 01:41:12 +0900

    gradle 설정

--- community-domain/src/main/java/kr/co/study/sample/web/SampleController.java
+++ community-domain/src/main/java/kr/co/study/sample/web/SampleController.java
@@ -3,8 +3,10 @@ package kr.co.study.sample.web;
 import kr.co.study.properties.DatabaseProperties;
 import kr.co.study.sample.dto.SampleDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by coupang on 2017. 5. 26..
@@ -14,6 +16,9 @@ public class SampleController {
 
     @Autowired
     DatabaseProperties properties;
+
+    @Autowired
+    private RestTemplateBuilder restTemplateBuilder;
 
     // Login Page
     @GetMapping("/")
